da5478a8e8bb54c8d73f1b3bbc2de58cadab6e8e
sam-dev97 <vivatecnosams@gmail.com>
2024-01-04 09:55:08 -0300

    Fixing wrong path

--- project/settings.py
+++ project/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-)f!ykxnc@jdz@05*uj#u8za$dg@i)k__n&rgc%()_gb$e&)8lg'
+SECRET_KEY = 'django-insecure-fndh_paj_gq2r@ulkz033gl9vye6+e-1x_*bqnb7s^(xxmqkx5'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
