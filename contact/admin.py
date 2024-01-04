45468f4f61dc91f3043d33ac3304e1da2a1a9a9a
sam-dev97 <vivatecnosams@gmail.com>
2024-01-04 15:20:31 -0300

    Added: Models

--- contact/admin.py
+++ contact/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
-
+from contact import models
 # Register your models here.
+
+@admin.register(models.Contact)
+class ContactAdmin(admin.ModelAdmin):
+    list_display = 'id','first_name', 'last_name', 'phone', 'email'
+    ordering = '-id',
+    #list_filter = 'created_date',
+    search_fields = 'id', 'fist_name', 'email',
+    list_per_page = 10
+    list_max_show_all = 200
+    list_editable = 'first_name', 'last_name', 'email', 'phone',
+    list_display_links = 'id',
