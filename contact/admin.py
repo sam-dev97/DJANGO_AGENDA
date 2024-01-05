4ca476a04e67b2f1d6f0a6765084236c707a737f
sam-dev97 <vivatecnosams@gmail.com>
2024-01-05 18:55:00 -0300

    new settings

--- contact/admin.py
+++ contact/admin.py
@@ -7,8 +7,15 @@ class ContactAdmin(admin.ModelAdmin):
     list_display = 'id','first_name', 'last_name', 'phone', 'email'
     ordering = '-id',
     #list_filter = 'created_date',
-    search_fields = 'id', 'fist_name', 'email',
+    search_fields = 'id', 'first_name', 'email',
     list_per_page = 10
     list_max_show_all = 200
     list_editable = 'first_name', 'last_name', 'email', 'phone',
     list_display_links = 'id',
+    
+    
+# Registro da categoria
+@admin.register(models.Category)
+class Category(admin.ModelAdmin):
+    list_display = 'name',
+    ordering = '-id',
--- contact/models.py
+++ contact/models.py
@@ -6,16 +6,31 @@ from django.utils import timezone
 # email, data de criação, descrição (texto)
 
 #category (forein key), show(boolean), owner(forein key) 
+class Category(models.Model):
+    #Mudar o nome da categoria la no admin do django
+    class Meta:
+        verbose_name = "Categoria"
+    
+    name = models.CharField(max_length=50)
+    
+    def __str__(self) -> str:
+        return self.name   
 
 class Contact(models.Model):
+    class Meta:
+        verbose_name = 'Contato'
+        verbose_name_plural = "Contatos"
+        
     #campo de texto comum
     first_name = models.CharField(max_length = 50)
     #campo de texto comum
-    last_name = models.CharField(max_length=50, blank=True)
+    last_name = models.CharField(max_length=50,
+                                blank=True)
     #campo de telefone em formato de texto comum
     phone = models.CharField(max_length=20)
     #campo de email
-    email = models.EmailField(max_length=254, blank=True)
+    email = models.EmailField(max_length=254,
+                            blank=True)
     #data de criação do cadastro
     created_date = models.DateTimeField(default=timezone.now)
     #descrição do usuário cadastrado
@@ -24,7 +39,16 @@ class Contact(models.Model):
     show = models.BooleanField(default=True)
     #adiciona um campo de adição de imagem
     #cria a pasta pictures e salva o arquivo dentro com ano e mês de adição
-    picture = models.ImageField(blank=True, upload_to='pictures/%Y/%m')
+    picture = models.ImageField(blank=True,
+                                upload_to='pictures/%Y/%m')
+    #Categoria com foreign key
+    category = models.ForeignKey(Category,
+                                on_delete=models.SET_NULL,
+                                blank=True,
+                                null=True
+                                )
+    
+    
     
     def __str__(self) -> str:
         return f'{self.first_name} {self.last_name}'
