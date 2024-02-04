c8cce29383e50beec28c305f28c5f0e7cae44448
sam-dev97 <vivatecnosams@gmail.com>
2024-02-04 17:31:18 -0300

    Adicionado: paginação, formulário de cadastro, novas páginas e organização do código.

--- contact/admin.py
+++ contact/admin.py
@@ -2,18 +2,19 @@ from django.contrib import admin
 from contact import models
 # Register your models here.
 
+
 @admin.register(models.Contact)
 class ContactAdmin(admin.ModelAdmin):
-    list_display = 'id','first_name', 'last_name', 'phone', 'email'
+    list_display = 'id', 'first_name', 'last_name', 'show'
     ordering = '-id',
-    #list_filter = 'created_date',
+    # list_filter = 'created_date',
     search_fields = 'id', 'first_name', 'email',
     list_per_page = 10
     list_max_show_all = 200
-    list_editable = 'first_name', 'last_name', 'email', 'phone',
+    list_editable = 'first_name', 'last_name', 'show'
     list_display_links = 'id',
-    
-    
+
+
 # Registro da categoria
 @admin.register(models.Category)
 class Category(admin.ModelAdmin):
--- contact/urls.py
+++ contact/urls.py
@@ -1,8 +1,15 @@
+# type:ignore
 from django.urls import path
+
 from contact import views
 
-app_name = 'contatc'
+app_name = 'contact'
 
 urlpatterns = [
-    path('',views.index, name='index'),
+    path('', views.index, name='index'),
+    path('search/', views.search, name='search'),
+
+    # contact (CRUD)
+    path('contact/<int:contact_id>/', views.contact, name='contact'),
+    path('contact/create/', views.create, name='create'),
 ]
